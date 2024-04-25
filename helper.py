7262f9edb7719741dcd63936a9cae555d8454523
jaison-jai-john <jaisonjaison2004@gmail.com>
2024-04-25 22:46:05 +0530

    added assert_type function which is used to handle invalid inputs

--- helper.py
+++ helper.py
@@ -9,3 +9,34 @@ def print_sep(sep=""):
 
 def clear_terminal():
     os.system("cls" if os.name == "nt" else "clear")
+
+
+def assert_type(
+    value,
+    type_=str,
+    message="Enter value: ",
+    error_message="invalid input",
+    take_value=False,
+    key=lambda x: x,
+):
+    while True:
+        try:
+            if take_value and not value:
+                value = type_(input(message))
+            else:
+                value = type_(value)
+            if not key(value):
+                raise ValueError
+            else:
+                clear_terminal()
+                break
+        except ValueError:
+            value = None
+            print(error_message)
+            choice = input("Do you want to try again? (y/n): ").lower()
+            if choice == "n":
+                clear_terminal()
+                return False
+            elif choice == "y":
+                pass
+    return value
